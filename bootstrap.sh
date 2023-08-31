190470b7ca676d8aa0ee9c469d5cba2fbbbedd10
Xunner <419848067@qq.com>
2023-08-31 15:38:29 +0800

    feat: update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,23 @@
 #! /usr/bin bash
-currentDir=~/home-server
+
 # 查找占用443端口的进程ID
-pid=$(sudo lsof -t -i:443)
+PREV_PID=$(sudo lsof -t -i:443)
 
 # 如果进程ID存在，发送SIGTERM信号给该进程
-if [[ -n $pid ]]; then
-    kill "$pid"
-    echo "SIGTERM signal sent to process $pid."
-else
-    echo "No process is using port 443."
+if [[ -n $PREV_PID ]]; then
+  OUTPUT=$(sudo kill "$PREV_PID")
+  if [ $? != 0 ]; then
+    echo "Prev program is running, killing failed: $OUTPUT"
+    exit 1
+  fi
+  echo "Prev program killed"
 fi
 
-nohup sudo go run $currentDir/main.go > output.log 2>&1 &
+if [ ! -d "output" ]; then
+  mkdir output
+fi
+CURRENT_DIR=~/home-server
+NOW=$(date '+%Y-%m-%d_%H:%M:%S')
+
+nohup sudo go run $CURRENT_DIR/main.go > output/output_"$NOW".log 2>&1 &
+cat output/output_"$NOW".log
